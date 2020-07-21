8f6874cd66978dde3d546040df4bf70002a40042
rabbiyasana <64980654+rabbiyasana@users.noreply.github.com>
2020-07-21 03:44:53 -0700

    done

---
+++ foot.js
@@ -0,0 +1,36 @@
+document.getElementById("footer").innerHTML =
+'<div class="container-fluid py-1" style="background-color: #007c88;">'+
+   '<div class="container">'+
+        '<div class="row">'+
+           '<div class="col-xl-4 col-md-6 col-sm-12 my-5">'+
+                '<span class="fa fa-phone-square fa-3x text-center d-block mx-auto my-2 text-light"></span>'+
+                '<p class="text-center font-weight-bold text-light">TALK TO US</p>'+
+                '<a href="#" class="text-light text-center d-block">Terms and Conditions</a><br>'+
+                '<a href="#" class="text-light text-center d-block">Disclaimer</a>'+
+            '</div>'+
+            '<div class="col-xl-4 col-md-6 col-sm-12 my-5">'+
+                '<span class="fa fa-envelope fa-3x text-center d-block mx-auto my-2 text-light"></span>'+
+                '<p class="text-center font-weight-bold text-light">E-Mail Us</p>'+
+                '<a href="#" class="text-light text-center d-block ">Privicy</a><br>'+
+                '<a href="#" class="text-light text-center d-block">How its Works</a>'+
+           '</div>'+
+           '<div class="col-xl-4 col-md-6 col-sm-12 my-5">'+
+                '<span class="fa fa-map-marker fa-3x text-center d-block mx-auto my-2 text-light"></span>'+
+                '<p class="text-center font-weight-bold text-light">Location</p>'+
+               '<p class="text-center text-light" >Microstrax software house <br>opposite Gp gate <br>UAF faisalabad</p>'+
+            '</div>'+
+     '</div>'+
+     '</div>'+
+'</div>'+
+'<div class="container-fluid mb-3"  style="background-color: #007c88;">'+
+    '<div class="container">'+
+        '<div class="row">'+
+        '<div class="col-xl-8 col-md-8 col-sm-12 my-2 text-center d-block">'+
+         '<img src="assets/img/BTC.png" alt="" class="l">  <img src="assets/img/ETH.png" alt="" class="l">  <img src="assets/img/LTC.png" alt="" class="l">  <img src="assets/img/cpay.png" alt="" class="l">'+
+        '</div>'+
+        '<div class="col-xl-4 col-md-4 col-sm-12 my-2 text-center d-block font-weight-bold">'+
+'@2019<span style="color: goldenrod;">Gold</span> <span style="color: white;">Ad</span> <span style="color:black;">Pack</span> <apan>.Com</span>'+
+        '</div>'+
+        '</div>'+
+    '</div>'+
+'</div>'
---
+++ nav.js
@@ -0,0 +1,25 @@
+document.getElementById("navMenu").innerHTML =
+'<div class="container-fluid" style="background-color: #007c88;">'+
+      '<div class="row">'+
+          '<div class="col-12">'+
+            '<nav class="navbar navbar-expand-md navbar-light py-3">'+
+            '<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">'+
+                '<span class="navbar-toggler-icon"></span>'+
+            '</button>'+
+            '<div class="collapse navbar-collapse  justify-content-center" id="navbarCollapse">'+
+                '<div class="navbar-nav">'+
+                    '<a href="index.html" class="nav-item nav-link text-light px-3">Home</a>'+
+                    '<a href="HOW.html" class="nav-item nav-link text-light px-3">How It Work</a>'+
+                    '<a href="#" class="nav-item nav-link text-light px-3">F.A.Q</a>'+
+                    '<a href="#" class="nav-item nav-link text-light px-3">Rules</a>'+
+                    '<a href="#" class="nav-item nav-link text-light px-3">Login</a>'+
+                    '<a href="#" class="nav-item nav-link text-light px-3">Contact</a>'+
+                '</div>'+ 
+            '</div>'+
+            '</nav>'+
+
+
+
+     '</div>'+
+    '</div>'+
+'</div>'
